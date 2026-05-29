b0c7770c3da9ec5b53f53e66b3f28572d034eb13
rraghadka <kabiyaaraghad@outlook.com>
2026-05-29 18:07:09 +0300

    add CONNECT request handling

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"encoding/binary"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net"
-	"io"
 	"os"
+	"sync"
 )
 
 func main() {
@@ -82,8 +84,17 @@ func handleConnection(conn net.Conn) {
 			return
 		}
 	}
+
+	target, err := handleConnect(conn)
+	if err != nil {
+		log.Printf("connect error: %v", err)
+		return
+	}
+	defer target.Close()
+
+	relay(conn, target)
 }
-	func authenticateUserPass(conn net.Conn) bool {
+func authenticateUserPass(conn net.Conn) bool {
 	header := make([]byte, 2)
 
 	_, err := io.ReadFull(conn, header)
@@ -133,4 +144,106 @@ func handleConnection(conn net.Conn) {
 
 	conn.Write([]byte{0x01, 0x01})
 	return false
+}
+func handleConnect(conn net.Conn) (net.Conn, error) {
+	header := make([]byte, 4)
+	if _, err := io.ReadFull(conn, header); err != nil {
+		return nil, err
+	}
+
+	if header[0] != 0x05 {
+		sendConnectReply(conn, 0x01)
+		return nil, fmt.Errorf("invalid SOCKS version")
+	}
+
+	if header[1] != 0x01 {
+		sendConnectReply(conn, 0x07)
+		return nil, fmt.Errorf("unsupported command")
+	}
+
+	if header[2] != 0x00 {
+		sendConnectReply(conn, 0x01)
+		return nil, fmt.Errorf("invalid reserved byte")
+	}
+
+	var host string
+
+	switch header[3] {
+	case 0x01:
+		addr := make([]byte, 4)
+		if _, err := io.ReadFull(conn, addr); err != nil {
+			return nil, err
+		}
+		host = net.IP(addr).String()
+
+	case 0x03:
+		lenBuf := make([]byte, 1)
+		if _, err := io.ReadFull(conn, lenBuf); err != nil {
+			return nil, err
+		}
+
+		domainLen := int(lenBuf[0])
+		domain := make([]byte, domainLen)
+
+		if _, err := io.ReadFull(conn, domain); err != nil {
+			return nil, err
+		}
+
+		host = string(domain)
+
+	default:
+		sendConnectReply(conn, 0x08)
+		return nil, fmt.Errorf("unsupported address type")
+	}
+
+	portBuf := make([]byte, 2)
+	if _, err := io.ReadFull(conn, portBuf); err != nil {
+		return nil, err
+	}
+
+	port := binary.BigEndian.Uint16(portBuf)
+	targetAddr := fmt.Sprintf("%s:%d", host, port)
+
+	target, err := net.Dial("tcp", targetAddr)
+	if err != nil {
+		sendConnectReply(conn, 0x05)
+		return nil, err
+	}
+
+	sendConnectReply(conn, 0x00)
+	return target, nil
+}
+
+func sendConnectReply(conn net.Conn, rep byte) {
+	conn.Write([]byte{
+		0x05,
+		rep,
+		0x00,
+		0x01,
+		0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00,
+	})
+}
+
+func relay(client net.Conn, target net.Conn) {
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+		io.Copy(target, client)
+		if tcp, ok := target.(*net.TCPConn); ok {
+			tcp.CloseWrite()
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		io.Copy(client, target)
+		if tcp, ok := client.(*net.TCPConn); ok {
+			tcp.CloseWrite()
+		}
+	}()
+
+	wg.Wait()
 }
